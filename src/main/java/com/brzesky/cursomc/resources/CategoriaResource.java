f42957c93b35c899df6eb1b5164ebcd1231baa50
NielsonBrzesky <nielsonbrzesky@gmail.com>
2020-04-13 19:08:40 -0300

    Testando a primeira parte de dominio.

--- src/main/java/com/brzesky/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/brzesky/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.brzesky.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brzesky.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource 
 {
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() 
+	public List<Categoria> listar() 
 	{
-		return "REST FUNCIONAL.";
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
