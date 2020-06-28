f48b496153642250ee0dd1095eb065eeb99bdc9e
NielsonBrzesky <nielsonbrzesky@gmail.com>
2020-06-28 20:22:44 -0300

    Teste de Primeira classe de dominio.

--- src/main/java/com/brzesky/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/brzesky/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.brzesky.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.brzesky.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource 
 {
 	@RequestMapping(method = RequestMethod.GET)
-	 public String listar() 
+	 public List<Categoria> listar() 
 	 {
-		 return "O REST ESTÁ FUNCIONANDO!!!";
+		Categoria catUm = new Categoria(1, "Informática");
+		Categoria catDois = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(catUm);
+		lista.add(catDois);
+		
+		return lista;
+//		 return "O REST ESTÁ FUNCIONANDO!!!";
 	 }
 }
