a7067b7737c4cf7908764ad26e4ba0ca3f1fa707
Nielson Brzesky <nielson.brzesky@el.com.br>
2020-07-25 00:42:59 -0300

    Deletando uma categoria com DELETE.

--- src/main/java/com/brzesky/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/brzesky/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.brzesky.cursomc.services.exceptions.DataIntegrityException;
 import com.brzesky.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -18,5 +19,13 @@ public class ResourceExceptionHandler
 		StandardError erro = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrityNotFound(DataIntegrityException e, HttpServletRequest request)
+	{
+		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
 	}
 }
--- src/main/java/com/brzesky/cursomc/services/CategoriaService.java
+++ src/main/java/com/brzesky/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.brzesky.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.brzesky.cursomc.domain.Categoria;
 import com.brzesky.cursomc.repositories.CategoriaRepository;
+import com.brzesky.cursomc.services.exceptions.DataIntegrityException;
 import com.brzesky.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,13 +31,24 @@ public class CategoriaService
 		return repositorio.save(obj);
 	}
 
-public Categoria update(Categoria obj)
-{
-	find(obj.getId());
-	return repositorio.save(obj);
-}
+	public Categoria update(Categoria obj)
+	{
+		find(obj.getId());
+		return repositorio.save(obj);
+	}
 
-
+	public void delete(Integer id)
+	{
+		find(id);//Chama o metodo find para verificar se o id existe se não ele entre nas classes de tratamento de excessão.
+		try 
+		{
+			repositorio.deleteById(id);			
+		}
+		catch (DataIntegrityViolationException e) 
+		{
+			throw new DataIntegrityException("Não é possivel excluir um valor com vinculos em outras tabelas!");
+		}
+	}
 
 }
 
