5b50863913e9c34fc20fea38b70722da44040b7a
Nielson Brzesky <nielson.brzesky@el.com.br>
2020-07-25 01:04:56 -0300

    Listando todas as categorias com o metodo DTO.

--- src/main/java/com/brzesky/cursomc/services/CategoriaService.java
+++ src/main/java/com/brzesky/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.brzesky.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,6 +49,11 @@ public class CategoriaService
 		{
 			throw new DataIntegrityException("Não é possivel excluir um valor com vinculos em outras tabelas!");
 		}
+	}
+	
+	public List<Categoria> findAll() 
+	{
+		return repositorio.findAll();
 	}
 
 }
