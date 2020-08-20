74fe468aa66124e10977778fb7ab5bfd7b953f17
Nielson Brzesky <nielson.brzesky@el.com.br>
2020-08-19 22:39:45 -0300

    Validação Customizada: email não repetido na inserção de CLiente.

--- src/main/java/com/brzesky/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/brzesky/cursomc/repositories/ClienteRepository.java
@@ -2,11 +2,13 @@ package com.brzesky.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.brzesky.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>
 {
-	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/brzesky/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/brzesky/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.brzesky.cursomc.domain.Cliente;
 import com.brzesky.cursomc.domain.enums.TipoCliente;
 import com.brzesky.cursomc.dto.ClienteNewDTO;
+import com.brzesky.cursomc.repositories.ClienteRepository;
 import com.brzesky.cursomc.resources.exception.FieldMessage;
 import com.brzesky.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> 
 {
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann){}
 
@@ -31,6 +38,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		{
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido!"));
 		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null)
+			list.add(new FieldMessage("email", "Email já existe!"));
 		
 		for (FieldMessage e : list) 
 		{
