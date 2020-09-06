a15eef27d4c310b2803f6e5e44a9aa34c8036acd
Nielson Brzesky <nielson.brzesky@el.com.br>
2020-09-05 21:33:35 -0300

    Adicionando senha a Cliente.

--- src/main/java/com/brzesky/cursomc/services/DBService.java
+++ src/main/java/com/brzesky/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.brzesky.cursomc.domain.Categoria;
@@ -33,6 +34,9 @@ import com.brzesky.cursomc.repositories.ProdutoRepository;
 @Service
 public class DBService 
 {
+	@Autowired
+	private BCryptPasswordEncoder pe;
+	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
@@ -123,7 +127,7 @@ public class DBService
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "nielsonbrzesky@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "nielsonbrzesky@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
